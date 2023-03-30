9f70c956dd0801a29f385a46c19e6b2f7a8a20a7
ProSweeper <dylan.doucet28@gmail.com>
2023-03-30 03:17:37 +0000

    finish basic blog

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
-import styles from './page.module.css'
+import styles from '../styles/home.module.css';
+import Link from 'next/link';
 
 export default function Home() {
   return (
-   <div>
-
+   <div className={styles.home}>
+      <div>
+        <h1>Hi My Name is Dylan</h1>
+        <div>
+          <Link href="/blog">Checkout My Blog</Link>
+        </div>
+        <div>
+          <Link href="/contact">Contact Me</Link>
+        </div>
+      </div>
    </div>
   )
 }
